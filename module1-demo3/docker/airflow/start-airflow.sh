feccd70f0bca7af8b99f99070ad50a2a73a5413b
msethi3 <msethi3@ext.uber.com>
2021-12-13 23:20:16 +0530

    Adding First Module

---
+++ module1-demo3/docker/airflow/start-airflow.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Move to the AIRFLOW HOME directory
+cd $AIRFLOW_HOME
+
+# Initiliase the metadatabase
+airflow initdb
+# shellcheck disable=SC2016
+airflow connections --add --conn_id 'data_path' --conn_type File --conn_extra '{ "path" : "data" }'
+airflow connections --add --conn_id 'postgres' --conn_type Postgres --conn_host 'postgres' --conn_login 'airflow' --conn_password 'airflow' --conn_schema 'xebia'
+airflow connections --add --conn_id 'slack_conn' --conn_type HTTP --conn_host 'https://hooks.slack.com/services' --conn_password "/T02JM63H2SE/B02R8GH0DU0/6sFXyfbXSJiAPfK4SjKYQymj"
+exec airflow webserver  &> /dev/null &
+
+exec airflow scheduler
+
